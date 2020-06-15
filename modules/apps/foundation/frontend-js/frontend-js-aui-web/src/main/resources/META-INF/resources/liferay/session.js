5ddb063362cb996976f1262a96b4cb2027de804b
binh.tran <binh.tran@codeenginestudio.com>
2020-06-15 14:44:38 +0700

    [TS][Prompt-3]Session extend popup didn't show properly on multiple tab

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
@@ -505,7 +505,7 @@ AUI.add(
 						instance._intervalId = host.registerInterval(
 							function(elapsed, interval, hasWarned, hasExpired, warningMoment, expirationMoment) {
 								if (!hasWarned) {
-									instance._uiSetActivated();
+									host.extend();
 								}
 								else if (!hasExpired) {
 									if (warningMoment) {
