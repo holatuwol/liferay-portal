09a3630aa7730dcf2a3ee866561a579dcad3cf16
DucHong VO <hong.vo@codeenginestudio.com>
2020-07-01 11:38:25 +0700

    LPS-115523 Revert "LPS-103738 Keep filter & order options when search using management toolbar"
    This reverts commit 65560e5bfc74b858b6716a2fcc3ceb3954ee0420.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
@@ -374,12 +374,6 @@ public class JournalManagementToolbarDisplayContext
 
 		portletURL.setParameter(
 			"folderId", String.valueOf(_journalDisplayContext.getFolderId()));
-		portletURL.setParameter(
-			"navigation", _journalDisplayContext.getNavigation());
-		portletURL.setParameter("orderByCol", getOrderByCol());
-		portletURL.setParameter("orderByType", getOrderByType());
-		portletURL.setParameter(
-			"status", String.valueOf(_journalDisplayContext.getStatus()));
 
 		return portletURL.toString();
 	}
