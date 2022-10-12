7abe5a68df02e2e6e81275c5bc2f0e97559e511d
tototrinh <to.trinh@codeenginestudio.com>
2022-10-12 12:17:03 +0700

    LPS-164942 User without permission should not see form container

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateConfigurationValuesMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateConfigurationValuesMVCActionCommand.java
@@ -179,7 +179,8 @@ public class UpdateConfigurationValuesMVCActionCommand
 				fragmentEntryLink, _portal.getHttpServletRequest(actionRequest),
 				_portal.getHttpServletResponse(actionResponse), layoutStructure)
 		).put(
-			"layoutData", layoutStructure.toJSONObject()
+			"layoutData",
+			LayoutStructureUtil.getLayoutDataJSONObject(layoutStructure)
 		).put(
 			"pageContents",
 			ContentUtil.getPageContentsJSONArray(
